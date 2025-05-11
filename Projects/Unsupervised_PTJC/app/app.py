fb06e3b63c6cf44e3ff39ef8c43d07a542ab8da3
bracken576 <62550662+bracken576@users.noreply.github.com>
2025-05-11 17:49:30 -0600

    updated unsupervised ptjc

--- Projects/Unsupervised_PTJC/app/app.py
+++ Projects/Unsupervised_PTJC/app/app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import re
 from runners.segmenting import seg_slices, display_slices, kmean_seg
 from runners.control_injured import run_model
-from runners.fibrosis_classification import unsupervised_clustering
+# from runners.fibrosis_classification import unsupervised_clustering
 
 
 st.set_page_config(page_title="Bio-Mechanical", layout="wide")
@@ -20,77 +20,6 @@ home, ml, segmentation, appendix = st.tabs(
     ]
 )
 
-
-ml_files = [None, "C053L", "C054L", "6012", "6013", "6139", "6140", "6315", "6342R", "6344R"]
-labels = [None, "Control", "Control", "Injured", "Injured", "Injured", "Injured", "Injured", "Injured", "Injured"]
-
-ml_locations_full = [
-    None,
-    "full_segmentation/Control/C053L",
-    "full_segmentation/Control/C054L",
-    "full_segmentation/Injured/6012",
-    "full_segmentation/Injured/6013",
-    "full_segmentation/Injured/6139",
-    "full_segmentation/Injured/6140",
-    "full_segmentation/Injured/6315",
-    "full_segmentation/Injured/6342R",
-    "full_segmentation/Injured/6344R",
-]
-ml_locations_part = [
-    None,
-    "partial_segmentation/Control/C053L",
-    "partial_segmentation/Control/C054L",
-    "partial_segmentation/Injured/6012",
-    "partial_segmentation/Injured/6013",
-    "partial_segmentation/Injured/6139",
-    "partial_segmentation/Injured/6140",
-    "partial_segmentation/Injured/6315",
-    "partial_segmentation/Injured/6342R",
-    "partial_segmentation/Injured/6344R",
-]
-ml_locations_kmean = [
-    None,
-    "kmean_segmentation/Control/C053L",
-    "kmean_segmentation/Control/C054L",
-    "kmean_segmentation/Injured/6012",
-    "kmean_segmentation/Injured/6013",
-    "kmean_segmentation/Injured/6139",
-    "kmean_segmentation/Injured/6140",
-    "kmean_segmentation/Injured/6315",
-    "kmean_segmentation/Injured/6342R",
-    "kmean_segmentation/Injured/6344R",
-]
-
-
-seg_files = [None, "C052L", "6342L", "6127L", "6126L", "6342R", "6344R"]
-
-seg_locations_full = [
-    None,
-    "full_segmentation/Control/C052L",
-    "full_segmentation/Control/6342L",
-    "full_segmentation/Injured/6127L",
-    "full_segmentation/Injured/6126L",
-    "full_segmentation/Injured/6342R",
-    "full_segmentation/Injured/6344R",
-]
-seg_locations_part = [
-    None,
-    "partial_segmentation/Control/C052L",
-    "partial_segmentation/Control/6342L",
-    "partial_segmentation/Injured/6127L",
-    "partial_segmentation/Injured/6126L",
-    "partial_segmentation/Injured/6342R",
-    "partial_segmentation/Injured/6344R",
-]
-seg_locations_kmean = [
-    None,
-    "kmean_segmentation/Control/C052L",
-    "kmean_segmentation/Control/6342L",
-    "kmean_segmentation/Injured/6127L",
-    "kmean_segmentation/Injured/6126L",
-    "kmean_segmentation/Injured/6342R",
-    "kmean_segmentation/Injured/6344R",
-]
 seg_bool = False
 
 
@@ -102,35 +31,10 @@ with home:
 
 with ml:
     st.markdown("## **Machine Learning**")
-    
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        file = st.selectbox("Test Images", options = ml_files, placeholder="Choose an option")
 
     seg = st.file_uploader("Upload a .nii or .nii.gz", type=["nii", "nii.gz", "gz"])
 
-    if file and not(seg) and not(seg_bool):
-        folder_dir = ml_files.index(file)
-        predicted = run_model(ml_locations_full[folder_dir])
-
-        st.markdown(f"#### Prediction: {predicted[0]} at {predicted[1]}% confidence")
-        st.markdown(f"#### Actual Label {labels[folder_dir]}")
-
-        display_slices(ml_locations_full[folder_dir])
-
-        if predicted[0] == "Injured" and labels[folder_dir] == "Injured":
-            my_dict = unsupervised_clustering()
-            display_slices(ml_locations_kmean[folder_dir])
-            cluster_df = pd.DataFrame.from_dict(my_dict)
-            cluster_df.columns = ["Image", "Cluster"]
-            cluster_df["Cluster"] = cluster_df["Cluster"].astype(int)
-            st.markdown(f"#### Image {file} classified in Cluster {cluster_df.loc[cluster_df['Image'] == file, 'Cluster'].iloc[0]}")
-            st.markdown("Cluster 0 typically has less dark green and I believe it is a lesser degree of capsule fibrosis.")
-            st.markdown("Cluster 1 typically has more dark green and I believe it is a higher degree of capsule fibrosis.")
-            st.markdown("I have not encountered a Cluster 2, but I would guess that it is a higher degree than Cluster 1.")
-
-
-    elif seg and not(seg_bool):
+    if seg and not(seg_bool):
         # Save the uploaded file if needed
         save_path = "user_slices/" + re.sub("^[^.]+", "user_image", seg.name)
         if os.path.exists(save_path):
@@ -149,16 +53,16 @@ with ml:
 
         display_slices("user_slices/full_segmentation")
 
-        if predicted[0] == "Injured":
-            my_dict = unsupervised_clustering()
-            display_slices(r'user_slices/kmean_segmentation')
-            cluster_df = pd.DataFrame.from_dict(my_dict)
-            cluster_df.columns = ["Image", "Cluster"]
-            # st.dataframe(cluster_df)
-            st.markdown(f"#### User Image classified in Cluster {cluster_df.loc[cluster_df['Image'] == 'user_slices/kmean_segmentation', 'Cluster'].iloc[0]}")
-            st.markdown("Cluster 0 typically has less dark green and I believe it has a lesser degree of capsule fibrosis.")
-            st.markdown("Cluster 1 typically has more dark green and I believe it has a higher degree of capsule fibrosis.")
-            st.markdown("I have not encountered a Cluster 2, but I would guess that it has a higher degree than Cluster 1.")
+        # if predicted[0] == "Injured":
+        #     my_dict = unsupervised_clustering()
+        #     display_slices(r'user_slices/kmean_segmentation')
+        #     cluster_df = pd.DataFrame.from_dict(my_dict)
+        #     cluster_df.columns = ["Image", "Cluster"]
+        #     # st.dataframe(cluster_df)
+        #     st.markdown(f"#### User Image classified in Cluster {cluster_df.loc[cluster_df['Image'] == 'user_slices/kmean_segmentation', 'Cluster'].iloc[0]}")
+        #     st.markdown("Cluster 0 typically has less dark green and I believe it has a lesser degree of capsule fibrosis.")
+        #     st.markdown("Cluster 1 typically has more dark green and I believe it has a higher degree of capsule fibrosis.")
+        #     st.markdown("I have not encountered a Cluster 2, but I would guess that it has a higher degree than Cluster 1.")
 
     if seg_bool:
         if os.path.exists("user_slices/user_image.nii"):
@@ -179,26 +83,12 @@ with ml:
 
 with segmentation:
     st.markdown("## **Segmentation**")
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        seg_file = st.selectbox("Test Images", options = seg_files, placeholder="Choose an option")
 
     image = st.file_uploader(
         "Upload a .nii or .nii.gz for segmentation", type=["nii", "nii.gz", "gz"]
     )
-    if seg_file and not(image) and not(seg_bool):
-        folder_dir = seg_files.index(seg_file)
-        
-        st.markdown("#### Full Segmentation")
-        display_slices(seg_locations_full[folder_dir])
 
-        st.markdown("#### Partial Segmentation")
-        display_slices(seg_locations_part[folder_dir])
-
-        st.markdown("#### Kmean Segmentation")
-        display_slices(seg_locations_kmean[folder_dir])
-
-    elif image:
+    if image:
         # Save the uploaded file if needed
         save_path = "user_slices/" + re.sub("^[^.]+", "user_image", image.name)
         if os.path.exists(save_path):
